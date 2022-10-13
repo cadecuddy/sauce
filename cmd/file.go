af29a9024a91b0e7b1f72220c419e184b13f9c20
Cade Cuddy <cadecuddy@gmail.com>
2022-10-13 01:05:30 -0400

    Implement function for searching by file

--- cmd/file.go
+++ cmd/file.go
@@ -1,10 +1,18 @@
 package cmd
 
 import (
+	"bytes"
 	"fmt"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"os"
 
+	"github.com/cadecuddy/sauce/utils"
 	"github.com/spf13/cobra"
 )
+
+const FILE_SEARCH = "https://api.trace.moe/search"
 
 // fileCmd represents the file command
 var fileCmd = &cobra.Command{
@@ -16,7 +24,34 @@ var fileCmd = &cobra.Command{
 - gif
 `,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("file called")
+		fileSearch(args[0])
 	},
 	Args: cobra.MinimumNArgs(1),
+}
+
+func fileSearch(filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("❌ Invalid file")
+		return
+	}
+	defer file.Close()
+
+	s := utils.ConfigSpinner()
+	s.Start()
+
+	// form file upload via https://gist.github.com/andrewmilson/19185aab2347f6ad29f5
+	buffer := &bytes.Buffer{}
+	writer := multipart.NewWriter(buffer)
+	part, _ := writer.CreateFormFile("file", filename)
+	io.Copy(part, file)
+	writer.Close()
+
+	res, err := http.Post(URL_SEARCH, writer.FormDataContentType(), buffer)
+	identifiedAnime, malData := utils.HandleResponse(res, err, s)
+
+	s.Stop()
+
+	// do things
+	utils.PrintSauce(identifiedAnime, malData.Data)
 }
